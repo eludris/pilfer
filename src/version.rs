72e487ed474062240330f57f53613c5cb7496b54
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-06-17 19:55:19 +0100

    refactor: compare versions with tuples

--- src/version.rs
+++ src/version.rs
@@ -1,16 +1,19 @@
-use std::io::{stdin, stdout, Write};
+use std::{cmp::Ordering, time::Duration};
 
 use lazy_static::lazy_static;
 use regex::Regex;
 use todel::models::InstanceInfo;
 
-const WARNING: &str =
-    "Warning: This version of Pilfer is more recent than the instance you are connecting to.
+const ERROR: &str =
+    "This version of Pilfer is more recent than the instance you are connecting to.
 
 Make sure you are using the right instance. An example of running Pilfer with a different instance is:
 
-    $ INSTANCE_URL=https://api.eludris.gay/next pilfer";
-const ERROR: &str = "This version of Pilfer is older than the instance you are connecting to.
+    $ INSTANCE_URL=https://api.eludris.gay/next pilfer
+
+If you are using the correct instance, downgrade pilfer.";
+const WARNING: &str =
+    "Warning: This version of Pilfer is older than the instance you are connecting to.
 
 Please update Pilfer to the latest version.";
 
@@ -74,32 +77,29 @@ pub fn check_version(info: &InstanceInfo) -> Result<(), String> {
         .map(|s| s.as_str())
         .unwrap_or("");
 
-    if instance_major < current_major
-        || instance_minor < current_minor
-        || instance_patch < current_patch
-        || current_prerelease != instance_prerelease
-    {
-        eprintln!("{}", WARNING);
-        print!("Continue anyway? (Y/n) > ");
-        stdout().flush().unwrap();
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-        let input = input.trim();
+    let instance = (
+        instance_major,
+        instance_minor,
+        instance_patch,
+        instance_prerelease,
+    );
+    let current = (
+        current_major,
+        current_minor,
+        current_patch,
+        current_prerelease,
+    );
 
-        loop {
-            match input {
-                "y" | "Y" | "" => break,
-                "n" | "N" => return Err("Aborted.".to_string()),
-                _ => {
-                    println!("Invalid input");
-                }
-            }
+    match instance.cmp(&current) {
+        Ordering::Equal => {}
+        Ordering::Greater => {
+            eprintln!("{}", ERROR);
+            return Err("Aborted.".to_string());
         }
-    } else if instance_major > current_major
-        || instance_minor > current_minor
-        || instance_patch > current_patch
-    {
-        return Err(ERROR.into());
+        Ordering::Less => {
+            eprintln!("{}", WARNING);
+            std::thread::sleep(Duration::from_secs(3));
+        }
     }
     Ok(())
 }
